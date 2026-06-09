1d23f8e0203b5d8a9198e8f70801e130ca2e3552
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2026-06-08 22:23:56 -0300

    feat: integração front x back

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 var body = document.querySelector("body")
 var singInButton = document.querySelector("#singIn")
 var singUpButton = document.querySelector("#singUp")
+const url = 'http://localhost:5016';
 
 body.onload = function()
 {
@@ -15,3 +16,121 @@ singInButton.addEventListener("click", function()
 singUpButton.addEventListener("click", function(){
     body.className = "sing-up"
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const registerButton = document.querySelector("#register");
+    const accessButton = document.querySelector("#access");
+
+    if (registerButton){
+        registerButton.addEventListener("click", formularioCadastrarUsuario);
+    }
+
+    if (accessButton){
+        registerButton.addEventListener("click", formularioAcessar);
+    }
+});
+
+const formularioCadastrarUsuario = async () => {
+    const email  = document.querySelector("#email").value.trim();
+    const password  = document.querySelector("#senha").value;
+    const confirmedPassword  = document.querySelector("#confirmarSenha").value;
+
+    if (email === ""){
+        alert("Email obrigatório");
+        return;
+    }
+
+    if (password === ""){
+        alert("Senha obrigatório");
+        return;
+    }
+
+    if (confirmedPassword === ""){
+        alert("Confirmar senha obrigatório");
+        return;
+    }
+
+    try{
+        let dto = JSON.stringify({
+            email,
+            password,
+            confirmedPassword
+        });
+
+        console.info('email', email);
+        console.info('dto', dto);
+        /*
+        {
+            "email": "user@teste.com",
+            "password": "Maudar@123",
+            "confirmedPassword": "Maudar@123"
+        }
+        */
+
+        const response = await fetch(url + "/Singup", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: dto,
+        });
+
+        if (!response.ok){
+            alert('Erro ao cadastrar usuário');
+            return;
+        }
+
+        alert('Sucesso ao cadastrar usuário');
+
+        //redirecionar para tela principal do sistema
+    } catch (error){
+        alert('Erro ao cadastrar usuário');
+        console.error(error.message);
+    }
+}
+
+const formularioAcessar = async () => {
+    const email  = document.querySelector("#email").value.trim();
+    const password  = document.querySelector("#senha").value;
+
+    if (email === ""){
+        alert("Email obrigatório");
+        return;
+    }
+
+    if (password === ""){
+        alert("Senha obrigatório");
+        return;
+    }
+
+    try{
+        let dto = JSON.stringify({
+            email,
+            password,
+            confirmedPassword
+        });
+
+        console.info('email', email);
+        console.info('dto', dto);
+
+        const response = await fetch(url + "/Singin", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: dto,
+        });
+
+        if (!response.ok){
+            alert('Erro ao acessar plataforma');
+            return;
+        }
+
+        alert('Sucesso ao acessar plataforma');
+
+        //redirecionar para tela principal do sistema
+    } catch (error){
+        alert('Erro ao acessar plataforma');
+        console.error(error.message);
+    }
+}
